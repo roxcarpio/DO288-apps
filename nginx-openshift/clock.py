182bca6f63ebd273a17e7a24e5bac6b3c5d8697a
Rosa María Carpio López <rmcarpio@atsistemas.com>
2021-04-22 14:18:42 +0200

    Docker image changed

--- nginx-openshift/clock.py
+++ nginx-openshift/clock.py
@@ -1,7 +1,16 @@
 import time
+import logging
+
+logging.basicConfig(filename="newfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 while True:
   localtime = time.localtime()
   result = time.strftime("%I:%M:%S %p", localtime)
   print(result)
+  logger.info("Just an information")
   time.sleep(1)
